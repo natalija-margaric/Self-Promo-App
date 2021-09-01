3b6c6ffd95755ae57fb5d66379a7bfb0592bf7e0
natalija-margaric <43842722+natalija-margaric@users.noreply.github.com>
2021-09-01 14:06:40 +0200

    PreviewActivity

---
+++ app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.selfpromoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
