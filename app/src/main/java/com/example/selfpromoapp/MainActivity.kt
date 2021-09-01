0941a32d5e002eb46b8f2e63ed928a795c2b7148
natalija-margaric <43842722+natalija-margaric@users.noreply.github.com>
2021-09-01 11:53:30 +0200

    kotlin

--- app/src/main/java/com/example/selfpromoapp/MainActivity.kt
+++ app/src/main/java/com/example/selfpromoapp/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.selfpromoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_preview.setOnClickListener {
+            onPreviewClicked()
+        }
+    }
+
+    private fun onPreviewClicked() {
+        val contactName = edit_text_contact_name.text.toString()
+        val contactNumber = edit_text_contact_number.text.toString()
+        val myDisplayName = edit_text_display_name.text.toString()
+        val includeJunior = check_box_include_junior.isChecked
+        val jobTitle = spinner_job_title.selectedItem.toString()
+        val immediateStart = check_box_immediate_start.isChecked
+        val startDate = edit_text_start_date.text.toString()
     }
 }
 
